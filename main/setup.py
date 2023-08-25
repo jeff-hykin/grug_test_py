15ce6f4c94133aae56e5c6d769b5419abd18bc01
Jeff Hykin <jeff.hykin@gmail.com>
2023-08-24 19:23:40 -0500

    slim down package

--- main/setup.py
+++ main/setup.py
@@ -31,6 +31,7 @@ setuptools.setup(
         package_info["name"]: [
             each[len(package_info["name"])+1:]
                 for each in iterate_paths_in(package_info["name"], recursively=True)
+                    if not each.endswith(".pyc") and not each.endswith("/.keep") and ('/settings/' not in each) and ('/commands/' not in each) and ('/documentation/' not in each)
         ],
     },
     install_requires=[
